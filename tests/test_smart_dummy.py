6d26e96b08c4c3f4483885c74271aee53f6b2519
Emma Zhang <emma.lzhang@gmail.com>
2023-06-23 15:22:47 -0500

    minor change to test input to scale up the input data size

--- tests/test_smart_dummy.py
+++ tests/test_smart_dummy.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from smart_dummy import get_dummies
 
-test_input = pd.DataFrame(['cat', 'dog', 'flower', 'tree', 'human', 'child'], columns=['category'])
+test_input = pd.DataFrame(['cat', 'dog', 'flower', 'tree', 'man', 'woman'] * 10000, columns=['category'])
 print(get_dummies(test_input['category'], 3))
