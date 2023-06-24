a5ceb1799062fe96aaccae72c2390915c8378f35
Emma Zhang <emma.lzhang@gmail.com>
2023-06-24 23:10:09 +0800

    update version to 0.2

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup, find_packages
 
 
-def parse_requirements(filename):
-    """Load requirements from a pip requirements file."""
-    with open(filename) as f:
-        lineiter = (line.strip() for line in f)
-        return [line for line in lineiter if line and not line.startswith("#")]
-
-
-reqs = parse_requirements('requirements.txt')
-
 setup(
     name="smart_dummy",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
-    install_requires=reqs,
-    author="Muriel Grobler; Emma Zhang",
-    author_email="muriel.grobler@gmail.com; emma.lzhang@gmail.com",
-    description="A smart and easy replacement to pandas.get_dummies() ",
+    install_requires=[
+        'numpy>=1.24',
+        'pandas>=1.0',
+        'pydantic>=1.10',
+        'scikit-learn>=1.0',
+        'spacy>=3.5'
+    ],
+    author="Muriel Grobler, Emma Zhang",
+    author_email="muriel.grobler@gmail.com, emma.lzhang@gmail.com",
+    description="A smart and easy alternative to pandas.get_dummies() ",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/emmalzhang/smart_dummy",
